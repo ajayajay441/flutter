5749ed76af0cb049f9c92c9df4836495dcbfa434
ajayajay441 <ajayajay.441@gmail.com>
2024-04-03 22:56:45 +0530

    chapter select

--- lib/ui/book/sidemenu_trigger.dart
+++ lib/ui/book/sidemenu_trigger.dart
@@ -16,19 +16,19 @@ Widget buildFAB(scaffoldKey, BuildContext context) {
         child: const Icon(Icons
             .menu_open), // Remove hero tag to prevent duplicate hero errors
       ),
-      const SizedBox(height: 8), // Add some vertical space between buttons
-      FloatingActionButton(
-          onPressed: () {
-            Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
-          },
-          backgroundColor:
-              const Color(0xFFA0642F), // Customize button color as needed
-          foregroundColor: Colors.white, // Customize icon color as needed
-          mini: true, // Make button smaller
-          child: const Icon(
-            Icons.menu_book_rounded,
-          ) // Remove hero tag to prevent duplicate hero errors
-          ),
+      // const SizedBox(height: 8), // Add some vertical space between buttons
+      // FloatingActionButton(
+      //     onPressed: () {
+      //       Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+      //     },
+      //     backgroundColor:
+      //         const Color(0xFFA0642F), // Customize button color as needed
+      //     foregroundColor: Colors.white, // Customize icon color as needed
+      //     mini: true, // Make button smaller
+      //     child: const Icon(
+      //       Icons.menu_book_rounded,
+      //     ) // Remove hero tag to prevent duplicate hero errors
+      //     ),
     ],
   );
 }
